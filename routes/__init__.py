ebd3d995798b45f4f604f463c89b620fa2968d26
Koji <129633452+Kazu0525@users.noreply.github.com>
2025-09-09 23:06:18 +0900

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,8 +1,8 @@
 # routes/__init__.py
-# 全ての相対インポートを絶対インポートに変更
+# 全ての相対インポートを絶対インポートに変更 + qrcode関連を完全除去
 
 from routes.redirect import router as redirect_router
-from routes.shorten import router as shorten_router
+from routes.shorten import router as shorten_router  
 from routes.analytics import router as analytics_router
 from routes.bulk import router as bulk_router
 from routes.export import router as export_router
